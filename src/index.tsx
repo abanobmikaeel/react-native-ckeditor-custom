88a053798742d6bfdcee6b93839c098ccd20db36
abanobmikaeel <abanobmikaeel@gmail.com>
2023-09-30 16:21:09 -0400

    Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Dimensions } from 'react-native';
 import { WebView, type WebViewMessageEvent } from 'react-native-webview';
